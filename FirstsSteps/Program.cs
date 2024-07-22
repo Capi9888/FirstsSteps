afe6efd8408b9b5b0dbc4fb0b66c86b1d4202377
sergi <sergi@CAPI>
2024-07-22 18:05:10 +0200

    Add project files.

---
+++ FirstsSteps/Program.cs
@@ -0,0 +1,138 @@
+﻿///////////////////////////////////////////////////////////////
+// First steps
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+// Consola
+///////////////////////////////////////////////////////////////
+
+/// La consola sirve para "imprimir algo por pantalla".
+/// Prueba a pulsar el botón verde de arriba con forma de "Play"
+/// Y verás como se te abre una nueva consola con el mensaje
+/// "Hello, World"
+
+Console.WriteLine("Hello, World!");
+
+/// Modifica este mensaje de Hello World y vuelve a ejecutar
+/// para ver como cambia. No olvides poner las comillas.
+/// Prueba a poner "Mi nombre es XXXX"
+/// Una vez practicado esto, comenta el método de arriba para que no se siga
+/// imprimiendo por pantalla añadiendo "//" delante del código,
+/// es decir, // Console.WriteLine("Hello, World!");
+
+
+///////////////////////////////////////////////////////////////
+// String
+///////////////////////////////////////////////////////////////
+
+/// Un string es como se le llama en programación a un caracter
+/// o conjunto de caracteres, ya sea una letra, una palabra o 
+/// una frase. Cualquier string debe inicializarse con un nombre.
+
+string firstStringEver = "Este es mi primer string";
+
+// **************** EJERCICIO 1 *******************************
+/// Los string pueden ser utilizados por otros "métodos", como 
+/// Console.WriteLine() que hemos visto antes. Prueba a CREAR
+/// un string e imprimirlo por pantalla.
+/// Resultado esperado: Darle al play y ver tu string por consola, es decir,
+/// si mi string es string nombre = "Javi"; por consola se deberá imprimir Javi
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ABAJO
+
+
+
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ARRIBA
+
+
+///////////////////////////////////////////////////////////////
+// Integer
+///////////////////////////////////////////////////////////////
+
+/// Un integer es como se le llama en programación a un número entero
+
+int firstNumberEver = 3;
+
+// **************** EJERCICIO 2 *******************************
+/// Prueba a CREAR
+/// un integer e imprimirlo por pantalla.
+/// Resultado esperado: Darle al play y ver tu integer por consola, es decir,
+/// si mi integer es integer numero = 7; por consola se deberá imprimir 7
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ABAJO
+
+
+
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ARRIBA
+
+
+///////////////////////////////////////////////////////////////
+// Operaciones con Integer
+///////////////////////////////////////////////////////////////
+
+/// Para realizar operaciones necesitas varios integer, las operaciones básicas son
+/// suma (+), resta (-), multiplicacion (*) y división (/)
+
+int primerNumero = 3;
+int segundoNumero = 7;
+int sumaDeNumeros = primerNumero + segundoNumero;
+
+// **************** EJERCICIO 3 *******************************
+/// Prueba a realizar una multiplicación de dos enteros e imprimirlo por pantalla.
+/// Resultado esperado: Darle al play y ver tu integer por consola, es decir,
+/// si mi primer integer es 7 y el segundo es 3, por consola se deberá imprimir 21
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ABAJO
+
+
+
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ARRIBA
+
+
+///////////////////////////////////////////////////////////////
+// Double
+///////////////////////////////////////////////////////////////
+
+/// Un double es como se le llama en programación a un número con decimales
+
+double firstDecimalsEver = 5.5;
+
+// **************** EJERCICIO 4 *******************************
+/// Prueba a CREAR
+/// un double e imprimirlo por pantalla.
+/// Resultado esperado: Darle al play y ver tu integer por consola, es decir,
+/// si mi integer es double decimal = 7.5; por consola se deberá imprimir 7.5
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ABAJO
+
+
+
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ARRIBA
+
+
+///////////////////////////////////////////////////////////////
+// Operaciones con double
+///////////////////////////////////////////////////////////////
+
+/// Para realizar operaciones necesitas varios double, las operaciones básicas son
+/// suma (+), resta (-), multiplicacion (*) y división (/)
+
+int primerDouble = 3;
+int segundoDouble = 7;
+int divisionDouble = primerNumero/segundoNumero;
+
+// **************** EJERCICIO 3 *******************************
+/// Prueba a realizar una resta de dos double e imprimirlo por pantalla.
+/// Resultado esperado: Darle al play y ver tu integer por consola, es decir,
+/// si mi primer double es 7.5 y el segundo es 3.5, por consola se deberá imprimir 4
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ABAJO
+
+
+
+
+// ESCRIBE EL CÓDIGO DEL EJERCICIO AQUÍ ARRIBA
